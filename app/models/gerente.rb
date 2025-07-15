fc991832972905cf57a3859a3a4229fb59be6515
Clemerson <clemesonsilva736@gmail.com>
2025-07-15 16:26:27 -0300

    desacoplamento de dependencias desnecessarias na entidade Gerente

--- app/models/gerente.rb
+++ app/models/gerente.rb
@@ -3,8 +3,6 @@ class Gerente < Funcionario
   has_many :motoristas, foreign_key: :supervisor_id
   has_many :fretes , through: :motoristas
   has_many :veiculos, through: :fretes
-  has_many :manutencaos, through: :veiculos
-  has_many :abastecimentos, through: :veiculos
 
   def get_fretes_supervisionados
     self.fretes
@@ -13,10 +11,10 @@ class Gerente < Funcionario
     self.motoristas.select(:id, :nome, :cpf)
   end
   def get_manutencoes_supervisionadas
-    self.manutencaos
+    self.veiculos.manutencaos
   end
   def get_abastecimentos_supervisionados
-    self.abastecimentos
+    self.veiculos.abastecimentos
   end
 
 
