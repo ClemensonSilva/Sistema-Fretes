be445b04fc7f9cbecfb489f7a95f3454e7973a7d
Clemerson <clemesonsilva736@gmail.com>
2025-07-16 08:30:22 -0300

    criacao de helpers nos respectivos modelos frete  emotorista

--- app/helpers/frete_helper.rb
+++ app/helpers/frete_helper.rb
@@ -1,2 +1,22 @@
 module FreteHelper
+  def frete_status_cor_classe(status)
+    case status.downcase
+    when "em_andamento"
+      'text-danger'
+    when "finalizado"
+      'text-success'
+    when "nao_iniciado"
+      'text-warning'
+    end
+  end
+  def frete_status_texto(status)
+    case status.downcase
+    when "em_andamento"
+      "Em andamento"
+    when "finalizado"
+      'Finalizado'
+    when "nao_iniciado"
+      'Não iniciado'
+    end
+  end
 end
--- app/helpers/motorista_helper.rb
+++ app/helpers/motorista_helper.rb
@@ -1,2 +1,5 @@
 module MotoristaHelper
+  def dados_motorista(motorista)
+    link_to "#{motorista.nome} - #{motorista.cpf}", funcionario_detalhes_path(motorista)
+  end
 end
