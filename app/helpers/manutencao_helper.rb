133c6ec316806f974448289065095807dbd850f8
Clemerson <clemesonsilva736@gmail.com>
2025-07-16 08:24:13 -0300

    configuracao das views show, detalhes  da entidade manutencao. Restam apenas refatorações

--- app/helpers/manutencao_helper.rb
+++ app/helpers/manutencao_helper.rb
@@ -1,2 +1,16 @@
 module ManutencaoHelper
+  def tipo_manutencao_cor_classe(tipo)
+    case tipo.downcase # Garante que a comparação seja insensível a maiúsculas/minúsculas
+    when 'preventiva'
+      'text-success' # Verde para preventiva
+    when 'corretiva'
+      'text-danger'  # Vermelho para corretiva
+    when 'preditiva'
+      'text-info'    # Azul claro para preditiva
+    when 'emergencial'
+      'text-warning' # Amarelo/laranja para emergencial
+    else
+      'text-secondary' # Cinza padrão para outros tipos ou não especificado
+    end
+  end
 end
