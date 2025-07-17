0b8c4f0d85f2b3f6be45615cc31a7d823fb634f9
Clemerson <clemesonsilva736@gmail.com>
2025-07-17 16:04:18 -0300

    funcionalidades basicas de crud da entidade Cnh

--- app/helpers/cnh_helper.rb
+++ app/helpers/cnh_helper.rb
@@ -1,2 +1,14 @@
 module CnhHelper
+  def cnh_status_cor_classe(status)
+    case status.downcase # Garante que a comparação seja insensível a maiúsculas/minúsculas
+    when 'ativa'
+      'text-success' # Verde para preventiva
+    when 'suspensa'
+      'text-warning'  # Vermelho para corretiva
+    when 'cassada'
+      'text-danger'
+    when 'vencida'
+      'text-secondary'
+    end
+  end
 end
