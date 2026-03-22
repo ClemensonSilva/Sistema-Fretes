ee4bc6e6e76cbc4c8922f1a2cb48c502ac4e1399
Clemerson <clemesonsilva736@gmail.com>
2026-03-22 12:25:28 -0300

    refactoração: fix melhorias nos layouts com as informações das entidades veiculo, abastecimento, manutencao, fretes

--- app/controllers/funcionario_controller.rb
+++ app/controllers/funcionario_controller.rb
@@ -1,7 +1,11 @@
 class FuncionarioController < ApplicationController
   before_action :authenticate_funcionario!
   def show
-    @funcionario = Funcionario.find(params[:id])
+    if current_funcionario.motorista?
+      @funcionario = current_funcionario
+    else
+      @funcionario = Funcionario.find(params[:id])
+    end
   end
   def edit
 
